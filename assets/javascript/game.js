26d94d6e0e60fd5cd223e25557f746854f653ef5
Braniel J. Pichardo <branieljose@gmail.com>
2016-12-06 23:53:02 -0500

    12/6/2016

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,90 @@
 $(document).ready(function() {
+  var goal;
   var losses = 0;
   var wins = 0;
-  
+  var total = 0;
+  var valueAssign = [ 
+     emerald = 0,
+     aquamarine = 0,
+     amethyst = 0,
+     opal = 0 
+      ];  
+  var images = [
+      $("<img>").addClass("emeraldImage").attr("src", "./assets/images/emerald.png").attr("data-emeraldValue", emerald) ,
+      $("<img>").addClass("aquamarineImage").attr("src", "./assets/images/aquamarine.png").attr("data-aquamaineValue", aquamarine) ,
+      $("<img>").addClass("amethystImage").attr("src", "./assets/images/amethyst.png").attr("data-amethystValue", amethyst) ,
+      $("<img>").addClass("opalImage").attr("src", "./assets/images/opal.png").attr("data-opalValue", opal)
+      ];
+      $('#crystalImages').append(images);
     
   function startGame() {
-    var total = 0;
-    var goal  = Math.floor(Math.random()*120);
-    var emerald = Math.floor((Math.random()*12)+1);
-    var aquamarine = Math.floor((Math.random()*12)+1);
-    var amethyst = Math.floor((Math.random()*12)+1);
-    var opal = Math.floor((Math.random()*12)+1); 
-      console.log(emerald);
-      console.log(aquamarine);
-      console.log(amethyst);
-      console.log(opal);
+    goal  = Math.floor(Math.random()*120);
     $('#goal').text(goal);
-    $("#currentScore").text(total);
-    $("#emerald").append(emeraldImage);
-  }
-  startGame();  
+    total = 0;
+    valueAssign = [ 
+     emerald = Math.floor((Math.random()*12)+1),
+     aquamarine = Math.floor((Math.random()*12)+1),
+     amethyst = Math.floor((Math.random()*12)+1),
+     opal = Math.floor((Math.random()*12)+1) 
+      ];     
+  };
+    
+  
+ $('.emeraldImage' ).on('click', function() {
+      total = total + emerald;    
+      $('#currentScore').text(total);
+      cases(); 
+  });
+ $('.aquamarineImage' ).on('click', function() {
+      total = total + aquamarine;      
+      $('#currentScore').text(total);
+      cases(); 
+  });
+ $('.amethystImage' ).on('click', function() {
+      total = total + amethyst;      
+      $('#currentScore').text(total);
+      cases(); 
+  });
+ $('.opalImage' ).on('click', function() {
+      total = total + opal;      
+      $('#currentScore').text(total);
+      cases(); 
+  });
+
+ function cases(){
+ if (total > goal){
+  losses++;
+  total = 0;
+  $('#currentScore').text(total);
+  $('#lost').text('You Lost! :(');
+  $('#losses').text(losses);
+  startGame();
+ } else if (total === goal){
+    wins++;
+    total = 0;
+    $('#currentScore').text(total);
+    $('#lost').text('You Won! :)');
+    $('#wins').text(wins);
+    startGame();
+ }
+};
+
+$('#button').append('<button>Reset</button>')
+$('button').on('click', function(){ 
+  total = 0;
+  $('#currentScore').text(total);
+  startGame();$('#lost').text('');
+});
+
+startGame();
 
 
-             
-          // if (total > goal) {
-          //   $('#losses').text(losses+=1);
-            
-          //     startGame();
-             
-                
-          // } else if ( total === goal) {
-          //   $('#wins').text(wins+=1);            
-            
-          //   startGame();
-          // } 
+
+  
+
+   
 
 
 
-    $("#button").on("click", function(){
-         
-         total = 0;
 
-    });
 });
